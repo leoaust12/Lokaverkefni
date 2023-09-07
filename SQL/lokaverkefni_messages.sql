f01a99e3434a5b91db6b022bdab0bd06a4f87795
leoaust12 <68393657+leoaust12@users.noreply.github.com>
2023-09-07 18:36:11 +0000

    Create lokaverkefni_messages.sql
    skilaboð!!!

---
+++ SQL/lokaverkefni_messages.sql
@@ -0,0 +1,5 @@
+CREATE TABLE messages (
+    AccountId int, -- I have no idea what I am doing here Ívan. I want this to make column get data from lokaverkefni_accounts.sql file.
+    MessageId int,
+    -- Add whatever you think might need.
+);
