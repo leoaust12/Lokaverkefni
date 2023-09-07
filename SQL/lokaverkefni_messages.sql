402daabdb3c1b4a04e7b98694e61e70d697c14c4
leoaust12 <68393657+leoaust12@users.noreply.github.com>
2023-09-07 21:21:10 +0000

    Update lokaverkefni_messages.sql
    Some addition and added comments

--- SQL/lokaverkefni_messages.sql
+++ SQL/lokaverkefni_messages.sql
@@ -1,5 +1,8 @@
+-- Everything in this table are a MUST
+
 CREATE TABLE messages (
     AccountId int, -- I have no idea what I am doing here Ívan. I want this to make column get data from lokaverkefni_accounts.sql file.
     MessageId int,
     -- Add whatever you think might need.
+    RecieverAccountId int, -- Whoever hes sending can only view the message.
 );
