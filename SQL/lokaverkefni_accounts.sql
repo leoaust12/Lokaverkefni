f4c718705bedbbb36855fb5d3c0e4628562757d9
leoaust12 <68393657+leoaust12@users.noreply.github.com>
2023-09-07 18:36:38 +0000

    Create lokaverkefni_accounts.sql
    accounts!!

---
+++ SQL/lokaverkefni_accounts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts (
+    AccountId int,
+    Username varchar(255),
+    FirstName varchar(255),
+    LastName varchar(255),
+    PhoneNumberCountry int,
+    PhoneNumber int,
+    -- You can edit this table if you want to.
+);
