4a3ee5fa0bf1994794fd69d52abd1c03b4fa26b6
leoaust12 <68393657+leoaust12@users.noreply.github.com>
2023-09-17 11:26:37 +0000

    Making React Work

---
+++ Backend/SQL/lokaverkefni_accounts.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 17, 2023 at 12:01 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `accounts-system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `ID` int(11),
+  `firstName` char(50),
+  `lastName` char(70),
+  `USER` varchar(255),
+  `PASS` varchar(255),
+  `EMAIL` varchar(255),
+  `GENDER` char(100),
+  `COUNTRY` char(56) NULL,
+  `PHONE` int(11) NULL,
+  `COUNTRYPHONEID` int(11) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
