0ce6755d2adadad236b6ab3501df252c1e7fcafe
leoaust12 <leoaustmannbaldursson@gmail.com>
2023-10-03 13:52:23 +0000

    Changes

---
+++ Front-End-Development/home-page/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css"
+
+export default function App() {
+    return(
+        <div className={"TopNavigation"}>
+            <img alt="Walter" src={"../../../Images/Walter.png"}/>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
