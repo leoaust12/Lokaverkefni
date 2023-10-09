689c5a87ed72ad3e40a80facb206b833ccfd92fa
leoaust12 <leoaustmannbaldursson@gmail.com>
2023-10-09 12:46:26 +0000

    Changes

--- Front-End-Development/Home/home.js
+++
@@ -1,5 +0,0 @@
-ReactDOM.render(
-    <div className={"Top"}>
-        <h1>Hello World</h1>
-    </div>,
-    document.getElementById("root"));
---
+++ Front-End-Development/Main/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+
+        </header>
+    )
+}
+
+const page = {
+    // Return a React Code
+}
+
+ReactDOM.render(page, document.getElementById("root"))
