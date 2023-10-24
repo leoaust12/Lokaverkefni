23d596722859a968a5c00b68f56a56f777d00f9e
GabrielMexico <ivanandri05@gmail.com>
2023-10-24 13:50:09 +0000

    Dreptu þig

--- Front-End-Development/home/src/Final.jsx
+++ Front-End-Development/home/src/Final.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Background_Video from "./components/Video.jsx";
 import Nav from "./components/Navigation.jsx";
+import Home from "./components/Home/home.jsx"
 import {Helmet} from "react-helmet";
 
 const title = "Welcome to SiteSpeak";
@@ -23,6 +24,7 @@ const Final = () => {
         <div>
             <MyComponent />
             <Nav />
+            <Home />
             <Background_Video />
         </div>
     )
