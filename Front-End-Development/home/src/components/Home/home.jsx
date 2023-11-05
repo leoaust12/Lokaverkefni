606dba2842e3240fb16da3cbccc6cc207606a13c
GabrielMexico <ivanandri05@gmail.com>
2023-11-05 13:57:07 +0000

    unnecessary files removed
    Video.jsx has been deleted and it's contents have been moved to home.jsx. Duplicates of home have been removed

--- Front-End-Development/home/src/components/Home/home.jsx
+++ Front-End-Development/home/src/components/Home/home.jsx
@@ -1,5 +1,7 @@
 import "./home.css";
 import Img from "../../assets/SiteSpeak.png";
+import "../Core/CSS/video.css";
+import Video from "../../assets/backgroundTest.mp4";
 
 const Home = () => {
     return(
@@ -7,6 +9,12 @@ const Home = () => {
             <img src={Img} id="sitespeak" />
             <h4 id="subtitle" >Empower your voice, preserve your privacy, connect freely</h4>
             <h4 id="signup">Sign Up</h4>
+
+            <div className="content">
+                <video src={Video} playsInline autoPlay muted loop>
+
+                </video>
+            </div>
         </div>
     )
 }
